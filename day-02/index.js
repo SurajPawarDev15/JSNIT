aab7291b7e7513848b974bf3070b9975ee0df5a5
SurajPawarDev15 <supersuraj143@gmail.com>
2026-05-02 23:50:15 +0530

    first commit

---
+++ day-02/index.js
@@ -0,0 +1,21 @@
+//let , var, const
+// var a = 10;
+// var a = 30;
+// console.log(" value of a  is " + a);
+
+// let a = 10;
+// a = 20;
+// console.log(a);
+
+// let a = 10;
+// let a = 20;
+// console.log(a);
+//SyntaxError: Identifier 'a' has already been declared
+
+// const a = 10;
+// console.log(a);
+
+// const a = 10;
+// a = 30;
+// console.log(a);
+//TypeError: Assignment to constant variable.
